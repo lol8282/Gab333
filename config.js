fcf860f008a2d8e909c45cd1f5d192d814563676
lol8282 <saranierogabriel@gmail.com>
2025-03-30 21:20:17 +0200

    Update config.

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ global.botnumber = ""
 global.confirmCode = ""
 
 global.owner = [
-    ['393762030481', 'Proprietario', true],
+    ['3515370786', 'Proprietario', true],
 ] //Numero degli owner
 
 //────────────────────────────
